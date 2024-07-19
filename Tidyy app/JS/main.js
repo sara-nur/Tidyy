1af47e8f876e4d9128515923d9467411ce5c2726
Sara Nuredinovski <saranur.cs@gmail.com>
2024-07-19 14:36:20 +0200

    feat Registration Form Fields  Validation- username validation, email and password validation

--- Tidyy app/JS/main.js
+++ Tidyy app/JS/main.js
@@ -1,33 +1,204 @@
+//ERROR COLORS
+let ErrorBackgroundColor = "#FE7D7D";
+let OkBackgroundColor = "#DFF6D8";
 
+//ERROR MESSAGES
 const errLogin = document.getElementById("error");
+const errRegister = document.getElementById("errorRegister");
+const errUsernameRegister = document.getElementById("errorUsernameRegister");
+const errEmailRegister = document.getElementById("errorEmailRegister");
+const errUsernameEmpty = document.getElementById("errorUsernameRegisterEmpty");
+const errEmailIncorrect = document.getElementById(
+  "errorEmailRegisterIncorrect"
+);
+const errEmailEmpty = document.getElementById("errorEmailRegisterEmpty");
+const errPasswordNotMatch = document.getElementById("errorPasswordRegister");
+const errPasswordEmpty = document.getElementById("errorPasswordRegisterEmpty");
+
+//VARIABLES FROM BUTTON FIELDS
 const loginBtn = document.getElementById("loginbtn");
+const registerBtn = document.getElementById("RegisterModalbtn");
 
-const users = ['sara.nur', 'ado', 'ramo'];
-const passwords = ['sara', 'ado', 'ramo'];
+//LISTS
+const usersList = ["sara.nur", "ado", "ramo"];
+const passwordsList = ["sara", "ado", "ramo"];
+const emailsList = ["sara.nur@gmail.com"];
 
+//VARIABLES
+let timeoutId;
 
+function ValidateUserLogin() {
+  let Username = document.getElementById("username").value.trim();
+  let Password = document.getElementById("password").value.trim();
+  let ispisiError = true;
 
-function ValidateUser(){
-    
-    let Username = document.getElementById("username").value.trim();
-    let Password = document.getElementById("password").value.trim();
-    let ispisiError= true;
-        
-        for(let i = 0; i<users.length; i++){
-            if(Username === users[i] && Password === passwords[i]){
-                window.location.href="home-screen.html";
-                ispisiError=false;
-        }}
+  for (let i = 0; i < usersList.length; i++) {
+    if (Username === usersList[i] && Password === passwordsList[i]) {
+      window.location.href = "home-screen.html";
+      ispisiError = false;
+    }
+  }
 
-        if(ispisiError){
-            errLogin.style.display = "block";
-        }
+  if (ispisiError) {
+    errLogin.style.display = "block";
+  }
 }
 
-if(loginBtn != null)
-{
-    loginBtn.addEventListener("click", (e)=>{
-        e.preventDefault();
-        ValidateUser();
-    });
+if (loginBtn != null) {
+  loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    ValidateUserLogin();
+  });
+}
+
+function RegisterUser() {
+  console.log(usersList);
+  console.log(passwordsList);
+  console.log(emailsList);
+
+  if (!ValidacijaUsera()) {
+    //window.location.href=
+  }
+}
+
+function ValidacijaUsera() {
+  const isUsernameValid = IsUsernameValid();
+  const isEmailValid = IsEmailValid();
+  const isPasswordEmpty = IsPasswordEmpty();
+  const isPasswordCorrect = IsPasswordCorrect();
+
+  if (isUsernameValid && isEmailValid && isPasswordEmpty && isPasswordCorrect) {
+    return false;
+  }
+  return true;
+}
+
+if (registerBtn != null) {
+  registerBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    RegisterUser();
+  });
+}
+
+function IsUsernameValid() {
+  const usernameRegistracija =
+    document.getElementById("usernameRegister").value;
+
+  if (usernameRegistracija === "") {
+    errUsernameEmpty.style.display = "block";
+    errUsernameRegister.style.display = "none";
+    return false;
+  } else {
+    errUsernameEmpty.style.display = "none";
+  }
+
+  let usernameExists = false;
+
+  for (let i = 0; i < usersList.length; i++) {
+    if (usernameRegistracija === usersList[i]) {
+      usernameExists = true;
+      break;
+    }
+  }
+
+  if (usernameExists) {
+    errUsernameRegister.style.display = "block";
+    return false;
+  } else {
+    errUsernameEmpty.style.display = "none";
+    return true;
+  }
+}
+
+function IsEmailValid() {
+  return (
+    !EmailInRegistrationFormEmpty() &&
+    !EmailInRegistrationFormExists() &&
+    EmailInRegistrationFormRegex()
+  );
+}
+
+function EmailInRegistrationFormEmpty() {
+  const emailRegistracija = document.getElementById("emali").value;
+
+  if (emailRegistracija === "") {
+    errEmailEmpty.style.display = "block";
+    errEmailRegister.style.display = "none";
+    errEmailIncorrect.style.display = "none";
+    return true;
+  } else {
+    errEmailEmpty.style.display = "none";
+    return false;
+  }
+}
+
+function EmailInRegistrationFormExists() {
+  const emailRegistracija = document.getElementById("emali").value;
+
+  let emailExists = false;
+
+  for (let i = 0; i < emailsList.length; i++) {
+    if (emailRegistracija === emailsList[i]) {
+      emailExists = true;
+      break;
+    }
+  }
+
+  if (emailExists) {
+    errEmailRegister.style.display = "block";
+    errEmailEmpty.style.display = "block";
+    errEmailIncorrect.style.display = "none";
+
+    return true;
+  } else {
+    errEmailRegister.style.display = "none";
+    return false;
+  }
+}
+
+function EmailInRegistrationFormRegex() {
+  const emailRegistracija = document.getElementById("emali").value;
+
+  const emailRegex = /^[a-zA-Z0-9.-_]+@[a-zA-Z]+.[a-zA-Z]{2,}$/;
+  if (!emailRegex.test(emailRegistracija)) {
+    errEmailIncorrect.style.display = "block";
+    errEmailRegister.style.display = "none";
+    errEmailEmpty.style.display = "none";
+    return false;
+  } else {
+    errEmailIncorrect.style.display = "none";
+    return true;
+  }
+}
+
+function IsPasswordEmpty() {
+  const passwordRegistracija =
+    document.getElementById("passwordRegister").value;
+  const passwordRegistracijaRepeat =
+    document.getElementById("password-repeat").value;
+
+  if (passwordRegistracija === "" || passwordRegistracijaRepeat === "") {
+    errPasswordEmpty.style.display = "block";
+    errPasswordNotMatch.style.display = "none";
+    return false;
+  } else {
+    errPasswordEmpty.style.display = "none";
+    return true;
+  }
+}
+
+function IsPasswordCorrect() {
+  const passwordRegistracija =
+    document.getElementById("passwordRegister").value;
+  const passwordRegistracijaRepeat =
+    document.getElementById("password-repeat").value;
+
+  if (passwordRegistracija !== passwordRegistracijaRepeat) {
+    errPasswordNotMatch.style.display = "block";
+    errPasswordEmpty.style.display = "none";
+    return false;
+  } else {
+    errPasswordNotMatch.style.display = "none";
+    return true;
+  }
 }
--- Tidyy app/JS/modal.js
+++ Tidyy app/JS/modal.js
@@ -3,128 +3,101 @@ const modalRegButton = document.getElementById("RegisterModalbtn");
 const createBtn = document.getElementById("createButton");
 const createMod = document.getElementById("createModal");
 
-if(createBtn != null)
-    createBtn.addEventListener("click",()=>{
-    createMod.style.display="block";
- });
+if (createBtn != null)
+  createBtn.addEventListener("click", () => {
+    createMod.style.display = "block";
+  });
 
-
- if(cancelButton != null)
-    cancelButton.addEventListener("click",()=>{
+if (cancelButton != null)
+  cancelButton.addEventListener("click", () => {
     console.log("I DID IT");
     createMod.style.display = "none";
- });
-
-
-
- if(modalRegButton != null)
-    modalRegButton.addEventListener("click",()=>{
-    createMod.style.display = "none";
- });
-
-
+  });
 
 const dropDownLists1 = document.getElementById("firstddl");
 const dropDownListContent1 = document.getElementById("firstContentddl");
-if(dropDownLists1 != null)
-dropDownLists1.addEventListener("click",()=>{
-    
-    if(dropDownListContent1.style.display =="block")
-        dropDownListContent1.style.display="none";
-    else if(dropDownListContent1.style.display="none")
-        dropDownListContent1.style.display="block";
-});
+if (dropDownLists1 != null)
+  dropDownLists1.addEventListener("click", () => {
+    if (dropDownListContent1.style.display == "block")
+      dropDownListContent1.style.display = "none";
+    else if ((dropDownListContent1.style.display = "none"))
+      dropDownListContent1.style.display = "block";
+  });
 
 const dropDownLists2 = document.getElementById("Secondddl");
 const dropDownListContent2 = document.getElementById("SecondContentddl");
-if(dropDownListContent2 != null)
-dropDownLists2.addEventListener("click",()=>{
-    
-    if(dropDownListContent2.style.display =="block")
-        dropDownListContent2.style.display="none";
-    else if(dropDownListContent2.style.display="none")
-        dropDownListContent2.style.display="block";
-});
+if (dropDownListContent2 != null)
+  dropDownLists2.addEventListener("click", () => {
+    if (dropDownListContent2.style.display == "block")
+      dropDownListContent2.style.display = "none";
+    else if ((dropDownListContent2.style.display = "none"))
+      dropDownListContent2.style.display = "block";
+  });
 
 const profileButton = document.getElementById("profileOptions");
-const profileDropDown= document.getElementById("proflieDropDown");
+const profileDropDown = document.getElementById("proflieDropDown");
 
-if(profileButton != null)
-    profileButton.addEventListener("click",()=>{
-        
-        if(profileDropDown.style.display =="block")
-            profileDropDown.style.display="none";
-        else if(profileDropDown.style.display="none")
-            profileDropDown.style.display="block";
-    });
+if (profileButton != null)
+  profileButton.addEventListener("click", () => {
+    if (profileDropDown.style.display == "block")
+      profileDropDown.style.display = "none";
+    else if ((profileDropDown.style.display = "none"))
+      profileDropDown.style.display = "block";
+  });
 
 const memberBtn = document.getElementById("Member");
 const memberModal = document.getElementById("memberMod");
 
-if(memberBtn != null)
-    memberBtn.addEventListener("click", ()=>{
-        memberModal.style.display = "block"
-    })
+if (memberBtn != null)
+  memberBtn.addEventListener("click", () => {
+    memberModal.style.display = "block";
+  });
 
-if(cancelButton != null)
-    cancelButton.addEventListener("click",()=>{
+if (cancelButton != null)
+  cancelButton.addEventListener("click", () => {
     memberModal.style.display = "none";
- });
-
-
+  });
 
 const modalddbtn = document.getElementById("modalddl");
 const modalddcontent = document.getElementById("modalcontentddl");
 
-
-if(modalddbtn != null)
-    modalddbtn.addEventListener("click",()=>{
-        
-        if(modalddcontent.style.display =="block")
-            modalddcontent.style.display="none";
-        else if(modalddcontent.style.display="none")
-            modalddcontent.style.display="block";
-    });
-
+if (modalddbtn != null)
+  modalddbtn.addEventListener("click", () => {
+    if (modalddcontent.style.display == "block")
+      modalddcontent.style.display = "none";
+    else if ((modalddcontent.style.display = "none"))
+      modalddcontent.style.display = "block";
+  });
 
 const modalddbtn2 = document.getElementById("modalddl2");
 const modalddcontent2 = document.getElementById("modalcontentddl2");
-    
-    
-    if(modalddbtn2 != null)
-        modalddbtn2.addEventListener("click",()=>{
-            
-            if(modalddcontent2.style.display =="block")
-                modalddcontent2.style.display="none";
-            else if(modalddcontent2.style.display="none")
-                modalddcontent2.style.display="block";
-        });
 
+if (modalddbtn2 != null)
+  modalddbtn2.addEventListener("click", () => {
+    if (modalddcontent2.style.display == "block")
+      modalddcontent2.style.display = "none";
+    else if ((modalddcontent2.style.display = "none"))
+      modalddcontent2.style.display = "block";
+  });
 
-        const modalddbtn3 = document.getElementById("modalddl3");
-        const modalddcontent3 = document.getElementById("modalcontentddl3");
-            
-            
-            if(modalddbtn3 != null)
-                modalddbtn3.addEventListener("click",()=>{
-                    
-                    if(modalddcontent3.style.display =="block")
-                        modalddcontent3.style.display="none";
-                    else if(modalddcontent3.style.display="none")
-                        modalddcontent3.style.display="block";
-                });
+const modalddbtn3 = document.getElementById("modalddl3");
+const modalddcontent3 = document.getElementById("modalcontentddl3");
 
+if (modalddbtn3 != null)
+  modalddbtn3.addEventListener("click", () => {
+    if (modalddcontent3.style.display == "block")
+      modalddcontent3.style.display = "none";
+    else if ((modalddcontent3.style.display = "none"))
+      modalddcontent3.style.display = "block";
+  });
 
 const teamBtn = document.getElementById("teamBtn");
 
-if(teamBtn != null)
-    teamBtn.addEventListener("click",()=>{
-        window.location.href = "team-overview.html";
-})
-
-function moveToOverview (){
+if (teamBtn != null)
+  teamBtn.addEventListener("click", () => {
     window.location.href = "team-overview.html";
+  });
+
+function moveToOverview() {
+  window.location.href = "team-overview.html";
 }
-
-
-
