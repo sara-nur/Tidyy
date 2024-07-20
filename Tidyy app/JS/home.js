99156dc5051c6efd1b6c5f45489c401958806607
Sara Nuredinovski <saranur.cs@gmail.com>
2024-07-20 13:03:39 +0200

    feat created guide for home screen

--- Tidyy app/JS/home.js
+++ Tidyy app/JS/home.js
@@ -1,62 +1,73 @@
+document.addEventListener("DOMContentLoaded", function () {
+  if (!localStorage.getItem("tourCompleted")) {
+    introJs().start();
 
-let listOfOrg = ["Test", "HCI"]
+    introJs().oncomplete(function () {
+      localStorage.setItem("tourCompleted", true);
+    });
 
-addEventListener("load",LoadOrg());
+    introJs().onexit(function () {
+      localStorage.setItem("tourCompleted", true);
+    });
+  }
+});
 
-function LoadOrg(){
-    let content = document.getElementById("firstContentddl").getElementsByTagName("li");
-    for(let i = 0; i<content.length; i++){
-        content[i].innerHTML="";
-    }
+let listOfOrg = ["Test", "HCI"];
 
-    for(let i = 0; i < listOfOrg.length; i++){
-        content[i].innerHTML += `
+addEventListener("load", LoadOrg());
+
+function LoadOrg() {
+  let content = document
+    .getElementById("firstContentddl")
+    .getElementsByTagName("li");
+  for (let i = 0; i < content.length; i++) {
+    content[i].innerHTML = "";
+  }
+
+  for (let i = 0; i < listOfOrg.length; i++) {
+    content[i].innerHTML += `
             <li>
                 <a href="orgainsation-screen.html">${listOfOrg[i]}</a>
             </li>
         `;
-        
-    }
+  }
 
-    arrList = JSON.stringify(listOfOrg);
-    localStorage.setItem("orgs",arrList);
-    console.log(arrList);
+  arrList = JSON.stringify(listOfOrg);
+  localStorage.setItem("orgs", arrList);
+  console.log(arrList);
 }
 
 const Button = document.getElementById("createBtn");
 
-Button.addEventListener("click", (e)=>{
-    e.preventDefault();
-    AddNewOrganisation();
+Button.addEventListener("click", (e) => {
+  e.preventDefault();
+  AddNewOrganisation();
 });
 
-function AddNewOrganisation()
-{
-    const nameInput = document.getElementById("OrgName").value.trim();
-    listOfOrg.push(nameInput);
-    arrList = JSON.stringify(listOfOrg);
-    localStorage.setItem("orgs",arrList);
-    LoadOrg();
+function AddNewOrganisation() {
+  const nameInput = document.getElementById("OrgName").value.trim();
+  listOfOrg.push(nameInput);
+  arrList = JSON.stringify(listOfOrg);
+  localStorage.setItem("orgs", arrList);
+  LoadOrg();
 
-    const createMod = document.getElementById("createModal");
-    createMod.style.display = "none";
+  const createMod = document.getElementById("createModal");
+  createMod.style.display = "none";
 }
 
+let content = document
+  .getElementById("firstContentddl")
+  .getElementsByTagName("a");
 
+for (let i = 0; i < content.length; i++) {
+  content[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(content[i].innerHTML);
+    SelectedOrg(content[i].innerHTML);
+    window.location.href = "orgainsation-screen.html";
+  });
+}
 
-
-let content= document.getElementById("firstContentddl").getElementsByTagName("a");
-
-    for(let i = 0; i < content.length; i++){
-        content[i].addEventListener("click",(e)=>{
-            e.preventDefault();
-            console.log(content[i].innerHTML);
-            SelectedOrg(content[i].innerHTML);
-            window.location.href="orgainsation-screen.html";
-        })
-    }
-
-
-function SelectedOrg(org){
-    localStorage.setItem("selectedOrganisation",org);
+function SelectedOrg(org) {
+  localStorage.setItem("selectedOrganisation", org);
 }
