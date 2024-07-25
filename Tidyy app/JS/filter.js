67a18aae2329e59fecf29ae855a6789ab932b8c2
Admir Pašić <pasicadmir5@gmail.com>
2024-07-25 17:18:49 +0200

    feat: filter tasks and subtasks

--- Tidyy app/JS/filter.js
+++ Tidyy app/JS/filter.js
@@ -5,61 +5,73 @@ const myTasks = document.getElementById("MyTasks");
 const searchBar = document.getElementById("search");
 
 const org = sessionStorage.getItem("org");
-const user = sessionStorage.getItem("loggedInUser");
+const User = sessionStorage.getItem("loggedInUser");
 const proj = sessionStorage.getItem("proj");
-const selectedTask = GetList("selectedTask");
 
 let project;
 let task;
 
+// function StringifyList( name, list){
+//     console.log(list);
+//     let newList = JSON.stringify(list);
+//     console.log(newList);
+//     sessionStorage.setItem(name,newList);
+// }
 
-let selectedColor = "#143444";
-let normalColor = "#012537";
-
-function StringifyList( name, list){
-    console.log(list);
-    let newList = JSON.stringify(list);
-    console.log(newList);
-    sessionStorage.setItem(name,newList);
-}
-
-function GetList(name){
-    let newList = sessionStorage.getItem(name);
-    console.log(newList);
-    return JSON.parse(newList);
-}
+// function GetList(name){
+//     let newList = sessionStorage.getItem(name);
+//     return JSON.parse(newList);
+// }
 
 
-if(User === "sara.nur"){
-    if(org === "HCI"){
-        project = GetList("HCIProject");
-        if(proj === "Project #1"){////////// MJENJAJ
-            task =GetList("Project1Tasks");
-        } 
-        else{
-            taskList = GetList("NewTasks");
-        }
-    }else{
-        project= GetList("NewProject");
-        task = GetList("NewTasks");
-    }
-}else{
-    project= GetList("NewProject");
-    task = GetList("NewTasks");
+// if(User === "sara.nur"){
+//     if(org === "HCI"){
+//         project = GetList("HCIProject");
+//         if(proj === "Project #1"){////////// MJENJAJ
+//             task =GetList("Project1Tasks");
+//         } 
+//         else{
+//             taskList = GetList("NewTasks");
+//         }
+//     }else{
+//         project= GetList("NewProject");
+//         task = GetList("NewTasks");
+//     }
+// }else{
+//     project= GetList("NewProject");
+//     task = GetList("NewTasks");
+// }
+
+const cards = document.getElementsByClassName("card");
+
+console.log(cards);
+
+for(let i = 0; i < cards.length; i++){
+    console.log(cards[i].getElementsByTagName("p")[2]);
 }
 
 if(allTasks != null && myTasks != null){
+
     allTasks.addEventListener("click", (e)=>{
         e.preventDefault();
-
-        
-        })
+        for(let i = 0; i < document.getElementsByClassName("card").length; i++){
+            if(cards[i].getElementsByTagName("p")[2].innerHTML !== `Asignee: ${User}`){
+                cards[i].style.display= "block"
+            }
+        }
+    })
+       
 
    
 
     myTasks.addEventListener("click", (e)=>{
-        e.preventDefault();
+        e.preventDefault();    
+        for(let i = 0; i < document.getElementsByClassName("card").length; i++){
+            if(cards[i].getElementsByTagName("p")[2].innerHTML !== `Asignee: ${User}`){
+                cards[i].style.display= "none";
+            }
+        }
         
     })
 
-};
+}
--- Tidyy app/JS/taskCards.js
+++ Tidyy app/JS/taskCards.js
@@ -4,15 +4,15 @@ const User = sessionStorage.getItem("loggedInUser");
 const proj = sessionStorage.getItem("proj");
 
 function StringifyList( name, list){
-    console.log(list);
+
     let newList = JSON.stringify(list);
-    console.log(newList);
+
     sessionStorage.setItem(name,newList);
 }
 
 function GetList(name){
     let newList = sessionStorage.getItem(name);
-    console.log(newList);
+
     return JSON.parse(newList);
 }
 
@@ -33,13 +33,11 @@ if(User === "sara.nur"){
   taskList = GetList("NewTasks");
 }
 
-console.log(taskList);
 const toDO = document.getElementById("to-do-column");
 const inProgress = document.getElementById("in-progress-column");
 const review = document.getElementById("code-review");
 const done = document.getElementById("done-column");
 
-console.log(taskList.length);
 
 for ( let i = 0; i < taskList.length; i++){
     if(taskList[i].progress === 0){
