9d225e5dab49ca57af56dd80adc92219f6b4e073
Sara Nuredinovski <saranur.cs@gmail.com>
2024-08-04 16:57:07 +0200

    fix color of paragraphs and headers

--- Tidyy app/JS/memberDelete.js
+++ Tidyy app/JS/memberDelete.js
@@ -2,59 +2,56 @@ const users = document.querySelectorAll(".team-card");
 const team = sessionStorage.getItem("team");
 
 function GetList(name) {
-    let newList = sessionStorage.getItem(name);
-    return JSON.parse(newList);
-  }
+  let newList = sessionStorage.getItem(name);
+  return JSON.parse(newList);
+}
 
-  function StringifyList(name, list) {
-    let newList = JSON.stringify(list);
-    sessionStorage.setItem(name, newList);
-  }
+function StringifyList(name, list) {
+  let newList = JSON.stringify(list);
+  sessionStorage.setItem(name, newList);
+}
 
-const list= GetList("TeamList");
+const list = GetList("TeamList");
 
 console.log(list);
 
 console.log(users);
 
-
-for(let i = 0; i < users.length; i++){
-    users[i].addEventListener("click", (e)=>{
-        
+for (let i = 0; i < users.length; i++) {
+  users[i].addEventListener("click", (e) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
         Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire({
-                title: "Deleted!",
-                text: "Your file has been deleted.",
-                icon: "success"
-              });
-              DeleteUser();
-            }
-          });
-    })
+          title: "Deleted!",
+          text: "You've successfully deleted this member!",
+          customClass: {
+            content: "my-custom-content-class",
+          },
+          icon: "success",
+        });
+      }
+    });
+  });
 }
 
-
-function DeleteUser(){
-    console.log(team);
-    for(let i = 0; i < list.length; i++){
-        if(list[i].teamName === team){
-            console.log(list[i]);
-            list[i].members.pop();
-
-        }
-        
+function DeleteUser() {
+  console.log(team);
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].teamName === team) {
+      console.log(list[i]);
+      list[i].members.pop();
     }
-    StringifyList("TeamList", list);
+  }
+  StringifyList("TeamList", list);
+  setTimeout(function () {
     window.location.reload();
+  }, 3000);
 }
-
-
--- Tidyy app/JS/team-overview.js
+++ Tidyy app/JS/team-overview.js
@@ -1,12 +1,9 @@
-
-
 const cards = document.querySelector(".team-layout");
 
-
 function GetList(name) {
-    let newList = sessionStorage.getItem(name);
-    return JSON.parse(newList);
-  }
+  let newList = sessionStorage.getItem(name);
+  return JSON.parse(newList);
+}
 
 const teams = GetList("TeamList");
 
@@ -16,55 +13,49 @@ console.log(teams);
 
 const teamNumber = teams.length;
 
-const memberNumber= getMemberNumber();
+const memberNumber = getMemberNumber();
 
-function getMemberNumber(){
-    let number = 0;
-    for(let i = 0; i < teams.length; i++){
-        if(teams[i].teamName === team){
-            console.log(teams[i])
-            number+= teams[i].members.length;
-        }
+function getMemberNumber() {
+  let number = 0;
+  for (let i = 0; i < teams.length; i++) {
+    if (teams[i].teamName === team) {
+      console.log(teams[i]);
+      number += teams[i].members.length;
     }
-    return number;
+  }
+  return number;
 }
-
 
 console.log(teamNumber);
 console.log(memberNumber);
 
-const counters = document.querySelectorAll(".team-counter")
+const counters = document.querySelectorAll(".team-counter");
 
 let numberOfMembers;
 
-
-
-cards.innerHTML = ""
-console.log(teams.length)
-for(let i = 0; i < teams.length; i++){
-    for(let j = 0; j < teams[i].members.length; j++){
-        numberOfMembers= teams[i].members.length;
-        if(teams[i].teamName === team){
-            cards.innerHTML +=
-            `
+cards.innerHTML = "";
+console.log(teams.length);
+for (let i = 0; i < teams.length; i++) {
+  for (let j = 0; j < teams[i].members.length; j++) {
+    numberOfMembers = teams[i].members.length;
+    if (teams[i].teamName === team) {
+      cards.innerHTML += `
             
             <div class="team-card">
             <p>${teams[i].members[j].name}</p>
             <p>${teams[i].members[j].username}</p>
             <p>${teams[i].members[j].email}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24"><path fill="currentColor" d="M7 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h4a1 1 0 1 1 0 2h-1.069l-.867 12.142A2 2 0 0 1 17.069 22H6.93a2 2 0 0 1-1.995-1.858L4.07 8H3a1 1 0 0 1 0-2h4zm2 2h6V4H9zM6.074 8l.857 12H17.07l.857-12zM10 10a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1m4 0a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24"><path fill="whitesmoke" d="M7 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h4a1 1 0 1 1 0 2h-1.069l-.867 12.142A2 2 0 0 1 17.069 22H6.93a2 2 0 0 1-1.995-1.858L4.07 8H3a1 1 0 0 1 0-2h4zm2 2h6V4H9zM6.074 8l.857 12H17.07l.857-12zM10 10a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1m4 0a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1"/></svg>
             </div>
             
             `;
-        }
     }
-    
+  }
 }
 
-for(let i = 0; i < counters.length; i++ ){
-    console.log(counters[i].innerHTML)
-    if(i == 0){
-        counters[i].innerHTML= `<p>Number of Members : ${memberNumber}</p>`;
-    }
+for (let i = 0; i < counters.length; i++) {
+  console.log(counters[i].innerHTML);
+  if (i == 0) {
+    counters[i].innerHTML = `<p>Number of Members : ${memberNumber}</p>`;
+  }
 }
-
