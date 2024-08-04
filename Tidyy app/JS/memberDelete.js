53bd93201723f15bb1f15a4df26b0dff2b7ae2c8
Admir Pašić <pasicadmir5@gmail.com>
2024-08-04 17:03:52 +0200

    fix: member delete button

--- Tidyy app/JS/memberDelete.js
+++ Tidyy app/JS/memberDelete.js
@@ -18,7 +18,8 @@ console.log(list);
 console.log(users);
 
 for (let i = 0; i < users.length; i++) {
-  users[i].addEventListener("click", (e) => {
+  users[i].addEventListener("click", (e)=>{
+  if(e.target.tagName === "svg"){
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -26,21 +27,16 @@ for (let i = 0; i < users.length; i++) {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
+      confirmButtonText: "Yes, delete it!"
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
           title: "Deleted!",
-          text: "You've successfully deleted this member!",
-          customClass: {
-            content: "my-custom-content-class",
-          },
-          icon: "success",
+          text: "Your file has been deleted.",
+          icon: "success"
         });
-      }
-    });
-  });
-}
+        DeleteUser();
+}})}})}
 
 function DeleteUser() {
   console.log(team);
@@ -53,5 +49,5 @@ function DeleteUser() {
   StringifyList("TeamList", list);
   setTimeout(function () {
     window.location.reload();
-  }, 3000);
+  }, 1000);
 }
